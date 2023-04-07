6a0718cfb87278e0b4055377615267be4bed2903
israil132415 <israil@socialadsignals.com>
2023-04-07 19:43:59 +0800

    Signed-off-by: israil132415 <israil@socialadsignals.com>

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,29 +1,25 @@
 "use client";
 
-import { signIn } from "next-auth/react";
-import axios from "axios";
 import { useCallback, useState } from "react";
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
-
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+import { useRouter } from "next/navigation";
 
-import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 import Modal from "./Modal";
-import Heading from "../Heading";
 import Input from "../inputs/Input";
-import { toast } from "react-hot-toast";
+import Heading from "../Heading";
 import Button from "../Button";
-import { useRouter } from "next/navigation";
 
 const LoginModal = () => {
   const router = useRouter();
-
-  const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
-
+  const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -39,6 +35,7 @@ const LoginModal = () => {
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
+
     signIn("credentials", {
       ...data,
       redirect: false,
@@ -46,7 +43,7 @@ const LoginModal = () => {
       setIsLoading(false);
 
       if (callback?.ok) {
-        toast.success("Welcome Back Master");
+        toast.success("Logged in");
         router.refresh();
         loginModal.onClose();
       }
@@ -57,14 +54,14 @@ const LoginModal = () => {
     });
   };
 
-  const toggle = useCallback(() => {
+  const onToggle = useCallback(() => {
     loginModal.onClose();
     registerModal.onOpen();
   }, [loginModal, registerModal]);
-  const bodyContent = (
-    <div className="flex flex-col gap4">
-      <Heading title="Welcome Back" subtitle="Login to your  account!" center />
 
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome back" subtitle="Login to your account!" />
       <Input
         id="email"
         label="Email"
@@ -96,29 +93,38 @@ const LoginModal = () => {
       />
       <Button
         outline
-        label="Continue with GitHub"
+        label="Continue with Github"
         icon={AiFillGithub}
         onClick={() => signIn("github")}
       />
-      <div className="text-neutral-500 text-center mt-4 font-light">
-        <div className="justify-center flex flex-row items-center gap-2">
-          <div>First time using Naomibnb?</div>
-          <div
-            onClick={toggle}
-            className="text-neutral-800 cursor-pointer hover:underline"
+      <div
+        className="
+      text-neutral-500 text-center mt-4 font-light"
+      >
+        <p>
+          First time using Airbnb?
+          <span
+            onClick={onToggle}
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
           >
-            Create an Account
-          </div>
-        </div>
+            {" "}
+            Create an account
+          </span>
+        </p>
       </div>
     </div>
   );
+
   return (
     <Modal
       disabled={isLoading}
       isOpen={loginModal.isOpen}
       title="Login"
-      actionLable="Continue"
+      actionLabel="Continue"
       onClose={loginModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
